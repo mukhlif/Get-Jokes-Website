ca54288dd5f707b30dd567ee4978bda7cfd19d80
mukhlif <asaadmohamad39@gmail.com>
2022-08-25 15:30:04 +0300

    jokes website

---
+++ app.js
@@ -0,0 +1,42 @@
+const jokeEl=document.getElementById('joke');
+const jokeBtn=document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click',getJoke);
+
+getJoke();
+
+async function getJoke(){
+
+    const config={
+        headers:{
+            Accept:'application/json',
+        },
+    }
+
+
+
+
+
+    const res=await fetch('https://icanhazdadjoke.com',config)
+    const data=await res.json();
+
+    jokeEl.innerHTML=data.joke;
+    }
+
+
+
+
+// function getJoke(){
+
+//     const config={
+//         headers:{
+//             Accept:'application/json',
+//         },
+//     }
+//     fetch('https://icanhazdadjoke.com',config)
+//     .then(res=>res.json())
+//     .then(data=>{
+//         jokeEl.innerHTML=data.joke;
+
+//     })
+// }
